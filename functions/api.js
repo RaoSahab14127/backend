5dcc34dbd83c8ef3d1715327ef1e729068631f06
raoSahab14127 <eb20103109.raohammadali@gmail.com>
2024-05-10 07:26:50 -0700

    fasf

--- functions/api.js
+++ functions/api.js
@@ -9,30 +9,30 @@ const app = express();
 dotenv.config();
 
 // Middleware
-// app.use(cors({ origin: "*", credentials: true }));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
-// const connectToMongoDB = async () => {
-//   try {
-//     const uri = process.env.MONGOURI;
-//     if (!uri) {
-//       throw new Error("MongoDB URI is not defined in environment variables");
-//     }
+const connectToMongoDB = async () => {
+  try {
+    const uri = process.env.MONGOURI;
+    if (!uri) {
+      throw new Error("MongoDB URI is not defined in environment variables");
+    }
 
-//     await mongoose.connect(uri);
-//     console.log("Connected to MongoDB!");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//     // Optionally, you might want to exit the application if MongoDB connection fails
-//     process.exit(1);
-//   }
-// };
-// connectToMongoDB();
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    // Optionally, you might want to exit the application if MongoDB connection fails
+    process.exit(1);
+  }
+};
+connectToMongoDB();
 
 // Use the router middleware function
-// app.use("/api", router);
+app.use("/api", router);
 
 // Export the app wrapped with serverless-http
-export default serverless(app);
+export const handler = serverless(app);
