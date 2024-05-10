7177c8328c38693c75d759a5beffd771937b9abf
raoSahab14127 <eb20103109.raohammadali@gmail.com>
2024-05-10 07:20:08 -0700

    dsffg

--- functions/api.js
+++ functions/api.js
@@ -14,22 +14,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
-const connectToMongoDB = async () => {
-  try {
-    const uri = process.env.MONGOURI;
-    if (!uri) {
-      throw new Error("MongoDB URI is not defined in environment variables");
-    }
+// const connectToMongoDB = async () => {
+//   try {
+//     const uri = process.env.MONGOURI;
+//     if (!uri) {
+//       throw new Error("MongoDB URI is not defined in environment variables");
+//     }
 
-    await mongoose.connect(uri);
-    console.log("Connected to MongoDB!");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    // Optionally, you might want to exit the application if MongoDB connection fails
-    process.exit(1);
-  }
-};
-connectToMongoDB();
+//     await mongoose.connect(uri);
+//     console.log("Connected to MongoDB!");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     // Optionally, you might want to exit the application if MongoDB connection fails
+//     process.exit(1);
+//   }
+// };
+// connectToMongoDB();
 
 // Use the router middleware function
 app.use("/api", router);
